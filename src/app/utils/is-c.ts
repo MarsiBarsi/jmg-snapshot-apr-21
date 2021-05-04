f97dcbaafc1802bcf0ef338e51fd25a2914a6047
MarsiBarsi <darragon-nn@yandex.ru>
2021-05-04 22:12:45 +0300

    chore: clarify variable

--- src/app/utils/is-c.ts
+++ src/app/utils/is-c.ts
@@ -1,5 +1,7 @@
 import MIDIMessageEvent = WebMidi.MIDIMessageEvent;
 
+const NOTES_PER_OCTAVE = 12;
+
 export function isC({data}: MIDIMessageEvent): boolean {
-    return !(data[1] % 12);
+    return !(data[1] % NOTES_PER_OCTAVE);
 }
